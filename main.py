4ff7efe01b8874c580099600406d3e097fbdbda4
dreyrd <andreyrosadias8@gmail.com>
2025-10-14 21:27:49 -0300

    arquivos iniciais

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PORT = int(os.getenv('PORT'))
+HOST = os.getenv('HOST')
+
+app = FastAPI(title='Stockapp-python')
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/teste')
+async def teste():
+    return {'Olaa': 'testee'}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host=HOST, port=PORT, log_level='debug', reload=True)
